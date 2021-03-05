9fc876b1bec56e3f0dcad990447d62c10211611d
rabbygit <rabbyhossain482@gmail.com>
2021-03-05 23:06:27 +0600

    routes file added and auth route created

---
+++ routes/routes.js
@@ -0,0 +1,33 @@
+/*
+ * Route defination of the application
+ *
+ */
+
+// Dependencies
+const authRoute = require('./authRoute');
+
+
+// Routes and their relative handler
+const routes = [
+    {
+        path: '/api/auth',
+        handler: authRoute
+    },
+    {
+        path: '/',
+        handler: (req, res) => {
+            res.status(200).send("Welcome To Library Management System.")
+        }
+    }
+]
+
+// Use the handler for related route
+module.exports = app => {
+    routes.forEach(route => {
+        if (route.path == '/') {
+            app.get(route.path, route.handler) // Root route
+        } else {
+            app.use(route.path, route.handler)
+        }
+    })
+}
